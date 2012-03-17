b75bd590ebd5491e57d1ecc04c121a65343131de
Francis Devereux <francis@devrx.org>
2012-03-17 18:33:04 +0000

    Made FileDownloadFormModel serializable to fix error:
    org.apache.wicket.util.io.SerializableChecker$WicketNotSerializableException: Unable to serialize class: com.brightinteractive.wicket.experiments.ui.FileDownloadFormModel

--- src/main/java/com/brightinteractive/wicket/experiments/ui/FileDownloadFormModel.java
+++ src/main/java/com/brightinteractive/wicket/experiments/ui/FileDownloadFormModel.java
@@ -4,10 +4,12 @@ package com.brightinteractive.wicket.experiments.ui;
  * Copyright 2012 Bright Interactive, All Rights Reserved.
  */
 
+import java.io.Serializable;
+
 /**
  * @author Bright Interactive
  */
-public class FileDownloadFormModel
+public class FileDownloadFormModel implements Serializable
 {
 	private String fileContents;
 
