b10d4b29225faf03293043b5b1f05cebc3f42509
Francis Devereux <francis@devrx.org>
2012-03-17 18:28:38 +0000

    Serve up a file when the file download form is submitted

--- src/main/java/com/brightinteractive/wicket/experiments/ui/FileDownloadForm.java
+++ src/main/java/com/brightinteractive/wicket/experiments/ui/FileDownloadForm.java
@@ -8,6 +8,10 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.handler.resource.ResourceStreamRequestHandler;
+import org.apache.wicket.request.resource.ContentDisposition;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.resource.StringResourceStream;
 
 /**
  * @author Bright Interactive
@@ -27,5 +31,17 @@ public class FileDownloadForm extends Form<FileDownloadFormModel>
 	{
 		FileDownloadFormModel model = this.getModelObject();
 		info("fileContents = " + model.getFileContents());
+
+        IResourceStream resourceStream = createResourceStreamWithContents(model.getFileContents());
+        getRequestCycle().scheduleRequestHandlerAfterCurrent(
+            new ResourceStreamRequestHandler(resourceStream, "yourfile.txt")
+                .setContentDisposition(ContentDisposition.ATTACHMENT)
+//                .setCacheDuration(cacheDuration)
+        );
 	}
+
+    private IResourceStream createResourceStreamWithContents(String contents)
+    {
+        return new StringResourceStream(contents, "text/plain");
+    }
 }
